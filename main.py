c84585ede7a1fb48900fba10eb1341079e17ca31
Matthew Rapati <mattrapati@gmail.com>
2017-08-19 23:08:26 -0400

    switch back to random template

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ twitterConfig = {
 
 # random template
 templateImageList = os.listdir('%s/templates' % dir)
-# templateImage = Image.open('%s/templates/%s' % (dir, random.choice(templateImageList)))
-templateImage = Image.open('%s/templates/matt_wizard_5.png' % dir)
+templateImage = Image.open('%s/templates/%s' % (dir, random.choice(templateImageList)))
+# templateImage = Image.open('%s/templates/matt_wizard_5.png' % dir)
 templateImage = templateImage.convert('RGBA')
 
 # random  fill image
